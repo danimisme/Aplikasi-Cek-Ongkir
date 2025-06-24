c81884e941c46a011a4c4199024996b461b38d12
Muhammad Subhan Ramdhani <msubhanr53@gmail.com>
2025-06-24 11:10:50 +0700

    Update HomeView to enhance dropdowns for province and city selection with improved UI and loading indicators

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -14,7 +14,7 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('HomeView'),
+        title: const Text('Cek Ongkir'),
         centerTitle: true,
       ),
       body: ListView(
@@ -70,6 +70,83 @@ class HomeView extends GetView<HomeController> {
               )
             ),
             onChanged: (value) => controller.cityAsalId.value = value?.cityId ?? "0",
+          ),
+          SizedBox(height: 20),
+          DropdownSearch<Province>(
+            compareFn: (item1, item2) => item1 == item2,
+            items: (f , s) => controller.fetchProvince(f),
+            itemAsString: (item) => item.province ?? '',
+            popupProps: PopupProps.menu(
+              showSelectedItems: true,
+              showSearchBox: true,
+              loadingBuilder: (context, searchEntry) => 
+                const Center(child: CircularProgressIndicator()),   
+              itemBuilder: (context, item, isDisabled , isSelected) {
+                return ListTile(
+                  title: Text(item.province ?? ''),
+                );
+              },
+            ),
+            decoratorProps: DropDownDecoratorProps(
+              decoration: InputDecoration(
+                labelText: "Provinsi Tujuan",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )
+            ),
+            onChanged: (value) => controller.provTujuanId.value = value?.provinceId ?? "0",
+          ),
+          SizedBox(height: 20),
+          DropdownSearch<City>(
+            compareFn: (item1, item2) => item1 == item2,
+            items: (f , s) => controller.fetchCity(f),
+            itemAsString: (item) => item.cityName ?? '',
+            popupProps: PopupProps.menu(
+              showSelectedItems: true,
+              showSearchBox: true,
+              loadingBuilder: (context, searchEntry) => 
+                const Center(child: CircularProgressIndicator()),   
+              itemBuilder: (context, item, isDisabled , isSelected) {
+                return ListTile(
+                  title: Text("${item.type} ${item.cityName}"),
+                );
+              },
+            ),
+            decoratorProps: DropDownDecoratorProps(
+              decoration: InputDecoration(
+                labelText: "Kota Tujuan",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              )
+            ),
+            onChanged: (value) => controller.cityTujuanId.value = value?.cityId ?? "0",
+          ),
+          SizedBox(height: 20),
+          DropdownSearch<Map<String, dynamic>>(
+            items: (f,s) => [
+              {"name": "JNE", "value": "jne"},
+              {"name": "TIKI", "value": "tiki"},
+              {"name": "POS Indonesia", "value": "pos"},
+            ],
+            compareFn: (item1, item2) => item1['value'] == item2['value'],
+            itemAsString: (item) => item['name'] ?? '',
+            popupProps: PopupProps.menu(
+              showSelectedItems: true,
+              itemBuilder: (context, item, isDisabled, isSelected) => 
+                ListTile(
+                  title: Text(item['name'] ?? ''),
+                ),
+            ),
+            decoratorProps: DropDownDecoratorProps(
+              decoration: InputDecoration(
+                labelText: "Pilih Kurir",
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                )
+              )
+            ),
           )
         ],
       ),
